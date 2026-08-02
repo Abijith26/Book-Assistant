d2627b7343a0b51199ba48431992cdcd00ed0d3f
Abijith26 <abimurugan.c@gmail.com>
2026-08-02 23:29:46 +0530

    created a rough navbar

---
+++ client/src/page-components/NavBar.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { cn as combineClasses } from "@/lib/utils";
+
+const navItems = [
+  { name: "Library", href: "/" },
+  { name: "Add New Book", href: "/books/new" },
+];
+
+function NavBar() {
+  const pathname = usePathname();
+  return (
+    <header className="w-full fixed z-50 bg-('--bg-primary')">
+      <div className="wrapper navbar-height py-4 flex justify-between items-center">
+        <Link href="/" className="flex gap-0.5 items-center">
+          <Image
+            src="/logo.png"
+            alt="Know-The-Book Logo"
+            width={42}
+            height={26}
+          />
+          <span className="logo-text">Know-The-Book</span>
+        </Link>
+        <nav className="w-fit flex gap-7.5 items-center">
+          {navItems.map((item) => {
+            const isActive = pathname === item.href;
+            function cn(): string | undefined {
+              throw new Error("Function not implemented.");
+            }
+
+            return (
+              <Link
+                key={item.href}
+                href={item.href}
+                className={combineClasses(
+                  "nav-link-base",
+                  isActive ? "nav-link-active" : "text-black hover:opacity-70",
+                )}
+              >
+                {item.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default NavBar;
